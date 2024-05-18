1e72bf4692bf9178ec6d44b32eaa4b99159e7278
Felipe-Morpheus <nocturne_dies@outlook.com>
2024-05-17 23:57:19 -0300

    implementando  cluster service01

---
+++ aws_project01/src/main/java/br/com/vulpicula/aws_project01/controller/TestController.java
@@ -0,0 +1,24 @@
+package br.com.vulpicula.aws_project01.controller;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/dog/{name}")
+    public ResponseEntity<?> dogTest(@PathVariable(name = "name") String dogName) {
+        LOG.info("Test controller - name: {}", dogName);
+
+        return ResponseEntity.ok("Name: " + dogName);
+    }
+}
+
---
+++ curso_aws_cdk/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,17 @@
+
+Y
+)src/main/java/com/myorg/ClusterStack.java,f\8\f8bc101c763b972bf71615bce8d8f2517470bb16
+[
++src/main/java/com/myorg/CursoAwsCdkApp.java,8\9\8981613e1987091257fc338e88b54f8dbfe7ac22
+]
+-src/main/java/com/myorg/CursoAwsCdkStack.java,0\0\00a01d0bb0cbae50f36409a78c1b61441d77228c
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+U
+%src/main/java/com/myorg/VpcStack.java,c\b\cb199853c319693a32e3764a7581ed654a6a915b
+[
++src/main/java/com/myorg/Service01Stack.java,4\7\47d41572144ac26845c8a76756e20ee5447bf3b7
+@
+cdk.context.json,6\1\61bc1575ef5319ef8ea9dc026b7df2e2543962e1
+8
+cdk.json,4\c\4cfd58be69b99d63dc5b0f5b434573a206386cd5
