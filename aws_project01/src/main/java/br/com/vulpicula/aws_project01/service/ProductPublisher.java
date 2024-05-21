fbcec0d4cb1b60b6c983c4f4dbf4c5be60fe7028
Felipe-Morpheus <nocturne_dies@outlook.com>
2024-05-21 04:00:23 -0300

    Consumindo os eventos de produtos com AWS SQS (02)

--- aws_project01/src/main/java/br/com/vulpicula/aws_project01/service/ProductPublisher.java
+++ aws_project01/src/main/java/br/com/vulpicula/aws_project01/service/ProductPublisher.java
@@ -16,38 +16,51 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductPublisher {
 
-    private static Logger LOG = LoggerFactory.getLogger(
+    // Define o logger estático para esta classe
+    private static Logger LOG = LoggerFactory.getLogger(ProductPublisher.class);
 
-            ProductPublisher.class);
-    private final AmazonSNS snsClient;
-    private final Topic productEventsTopic;
-    private final ObjectMapper objectMapper;
+    // Declaração das dependências
+    private final AmazonSNS snsClient; // Cliente SNS da AWS
+    private final Topic productEventsTopic; // Tópico SNS de eventos de produto
+    private final ObjectMapper objectMapper; // Objeto ObjectMapper para conversão de objetos Java para JSON
 
+    // Construtor da classe ProductPublisher
     public ProductPublisher(AmazonSNS snsClient,
                             @Qualifier("productEventsTopic") Topic productEventsTopic,
                             ObjectMapper objectMapper) {
-        this.snsClient = snsClient;
-        this.productEventsTopic = productEventsTopic;
-        this.objectMapper = objectMapper;
+        // Inicializa as dependências
+        this.snsClient = snsClient; // Inicializa o cliente SNS
+        this.productEventsTopic = productEventsTopic; // Inicializa o tópico SNS
+        this.objectMapper = objectMapper; // Inicializa o ObjectMapper
     }
 
+    /**
+     * Publishes product events to the SNS topic.
+     *
+     * @param product   The product for which the event is being published.
+     * @param eventType The type of event (CREATE, UPDATE, DELETE).
+     * @param username  The username of the user initiating the event.
+     */
     public void publishProductEvents(Product product, EventType eventType, String username) {
+        // Cria um objeto ProductEvent com os detalhes relevantes
         ProductEvent productEvent = new ProductEvent();
         productEvent.setProductId(product.getId());
         productEvent.setCode(product.getCode());
         productEvent.setUsername(username);
 
+        // Cria um objeto Envelope para envolver o ProductEvent
         Envelope envelope = new Envelope();
         envelope.setEventType(eventType);
 
         try {
+            // Converte os objetos Envelope e ProductEvent em strings JSON
             envelope.setData(objectMapper.writeValueAsString(productEvent));
 
-            snsClient.publish(
-                    productEventsTopic.getTopicArn(),
-                    objectMapper.writeValueAsString(envelope));
+            // Publica a mensagem JSON no tópico SNS
+            snsClient.publish(productEventsTopic.getTopicArn(), objectMapper.writeValueAsString(envelope));
 
         } catch (JsonProcessingException e) {
+            // Registra um erro se houver algum problema ao criar a mensagem JSON
             LOG.error("Failed to create product event message");
         }
     }
